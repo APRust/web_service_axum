202ff2b5b1dbffb20c38d0103edced300e9b8b72
APRust <alex.pikalov.rust@gmail.com>
2024-11-11 16:26:21 +0300

    add CRUD rest api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+pub use self::error::{Error, Result};
+use crate::model::ModelController;
 use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
@@ -8,16 +10,19 @@ use serde::Deserialize;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
-
-pub use self::error::{Error, Result};
 mod error;
+mod model;
 mod web;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    // Initialize ModelController
+    let mc = ModelController::new().await?;
+
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
+        .nest("/api", web::routes_tickets::routes(mc.clone()))
         .layer(middleware::map_response(main_response_mapper))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
@@ -31,6 +36,7 @@ async fn main() {
         .unwrap();
 
     // endregion: --- Start Server
+    Ok(())
 }
 
 async fn main_response_mapper(res: Response) -> Response {
